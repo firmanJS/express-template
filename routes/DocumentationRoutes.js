57dd9b686cfbd5e2d1a41b2d6dd04e2d01b7467e
firmanjs <fimzhakim@gmail.com>
2021-05-31 14:40:42 +0700

    updated dockerfile node version

--- routes/DocumentationRoutes.js
+++ routes/DocumentationRoutes.js
@@ -11,7 +11,14 @@ router.get(`${API_PATH}/documentation`, swaggerUi.setup(index))
 router.get('/', (req, res) => {
   customResponse(res, 200, 'Api running', {
     welcome: 'Welcome to Boilerplate API.',
-    documentation: `http://${req.get('host')}/api/v1/documentation`
+    hostname: require('os').hostname(),
+    uptime: require('os').uptime(),
+    loadavg: require('os').loadavg(),
+    totalmem: require('os').totalmem(),
+    version: require('os').version(),
+    platform: require('os').platform(),
+    release: require('os').release(),
+    documentation: `http://${req.get('host')}/api/v1/documentation`,
   })
 })
 module.exports = router
