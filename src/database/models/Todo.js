c91b35e4b02b3d52eb24347b841f7b30139f8e4b
FirmanTS <fimzhakim@gmail.com>
2021-12-15 15:24:01 +0700

    updated data postgres

--- src/database/models/Todo.js
+++ src/database/models/Todo.js
@@ -4,11 +4,6 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate() {
       // define association here
     }
