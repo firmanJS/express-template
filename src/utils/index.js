0d462dbb201fe89d529bba8e5270fa24cb734bff
FirmanTS <fimzhakim@gmail.com>
2021-12-20 11:03:38 +0700

    raise coverage

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,12 +2,10 @@ const constant = require('./constant')
 const custom = require('./custom')
 const exceptions = require('./exceptions')
 const pagination = require('./pagination')
-const validation = require('./validation')
 
 module.exports = {
   ...constant,
   ...custom,
   ...exceptions,
-  ...pagination,
-  ...validation
+  ...pagination
 }
